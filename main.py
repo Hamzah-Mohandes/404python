02bdbba8b4cd1de1a174feb7f20407e4977d1d2d
Hamzah-Mohandes <rootmoodrm@gmail.com>
2023-11-27 14:00:53 +0100

    s = input("just for fun give me a sentence : -> ") mid = len(s)//2 print(s[mid])

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-def string_methods_practice():
-    # Eingabe eines Strings
-    input_string = input("Geben Sie einen beliebigen Text ein: ")
+# def string_methods_practice():
+#     # Eingabe eines Strings
+#     input_string = input("Geben Sie einen beliebigen Text ein: ")
+#
+#     # Anwenden der String-Methoden
+#     print("capitalize():", input_string.capitalize())
+#     print("lower():", input_string.lower())
+#     print("upper():", input_string.upper())
+#     print("title():", input_string.title())
+#     print("swapcase():", input_string.swapcase())
+#     print("strip():", input_string.strip())
+#     print("split():", input_string.split())
+#     print("replace('a', 'b'):", input_string.replace('a', 'b'))
+#     print("find('e'):", input_string.find('e'))
+#     print("startswith('H'):", input_string.startswith('H'))
+#     print("endswith('d'):", input_string.endswith('d'))
+#     print("len():", len(input_string))
+#
+# string_methods_practice()
 
-    # Anwenden der String-Methoden
-    print("capitalize():", input_string.capitalize())
-    print("lower():", input_string.lower())
-    print("upper():", input_string.upper())
-    print("title():", input_string.title())
-    print("swapcase():", input_string.swapcase())
-    print("strip():", input_string.strip())
-    print("split():", input_string.split())
-    print("replace('a', 'b'):", input_string.replace('a', 'b'))
-    print("find('e'):", input_string.find('e'))
-    print("startswith('H'):", input_string.startswith('H'))
-    print("endswith('d'):", input_string.endswith('d'))
-    print("len():", len(input_string))
+s = input("just for fun give me a sentence : -> ")
+mid = len(s)//2
+print(s[mid])
 
-string_methods_practice()
+
+
