84c4b50b605c908b1cfc74ba5f4510e7b7fea4c1
Hamzah-Mohandes <rootmoodrm@gmail.com>
2023-12-07 19:12:50 +0100

    gui and console fertig :D

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# Ein einfaches Konsolen-Kassenprogramm in Python ohne Verwendung von Bibliotheken
+import tkinter as tk
+from tkinter import messagebox
+
 
 # Ein Wörterbuch, um die Speisekartenartikel und ihre Preise zu speichern
 menu = {
@@ -19,85 +21,142 @@ menu = {
 # Ein Wörterbuch, um die Bestellungen für jeden Tisch zu speichern
 orders = {}
 
-# Funktion zum Anzeigen der Speisekartenartikel und ihrer Preise
-def display_menu():
-    print("Liste und Preise der Produkte:")
-    for category, items in menu.items():
-        print(f"{category}:")
-        for index, (item, price) in enumerate(items.items(), start=1):
-            print(f"   {index}. {item}: {price} Toman")
-        print()
+# Funktion zum Anzeigen der GUI
+def main_gui():
+    global root
+    root = tk.Tk()
+    root.title("Kassenprogramm")
 
-# Funktion zum Anzeigen der Artikel für eine bestimmte Tischnummer
-def display_items_for_table(table):
-    if table in orders:
-        print(f"Bestellungen für Tisch {table}:")
-        for index, (category, item) in enumerate(orders[table], start=1):
-            print(f"{index}. {item} aus der Kategorie {category}")
-    else:
-        print(f"Die Tischnummer {table} existiert nicht.")
+    load_orders_from_file()
+    display_start_options_gui()
 
-# Funktion zum Löschen eines Artikels für eine bestimmte Tischnummer
-def delete_item_for_table(table):
-    display_items_for_table(table)
-    if table in orders:
-        try:
-            item_index = int(input("Geben Sie die Nummer des zu löschenden Artikels ein: ")) - 1
-            if 0 <= item_index < len(orders[table]):
-                deleted_item = orders[table].pop(item_index)
-                print(f"Der Artikel {deleted_item[1]} aus der Kategorie {deleted_item[0]} wurde gelöscht.")
-            else:
-                print("Ungültige Artikelnummer.")
-        except ValueError:
-            print("Ungültige Eingabe. Bitte geben Sie eine Nummer ein.")
-    else:
-        print(f"Die Tischnummer {table} existiert nicht.")
+    root.mainloop()
 
-# Funktion zum Anzeigen der Optionen für den Tischwechsel
-def display_table_change_options():
-    print("Optionen für Tischwechsel:")
-    print("1. Tisch wechseln")
-    print("2. Name des Tisches ändern")
-    print("3. Tisch löschen")
-    print("4. Zurück zur Startseite")
 
-# Funktion zum Anzeigen der Optionen für den Kunden
-def display_start_options():
-    print("Startseite:")
-    print("1. Produkt bestellen")
-    print("2. Rechnung anzeigen")
-    print("3. Tisch wechseln")
-    print("4. Bestellung löschen oder bearbeiten")
-    print("5. Gesamte Rechnungen anzeigen")
-    print("6. Programm verlassen")
+# Funktion zum Laden der Bestellungen aus einer Textdatei
+def load_orders_from_file():
+    global orders
+    try:
+        with open("bestellungen.txt", "r") as file:
+            orders_data = file.readlines()
+            for order in orders_data:
+                order_parts = order.strip().split(": ")
+                table = order_parts[0].replace("Tisch ", "")
+                category, item = order_parts[1].split(" ")
+                orders.setdefault(table, []).append((category, item))
+    except FileNotFoundError:
+        print("Die Datei 'bestellungen.txt' wurde noch nicht erstellt.")
 
-# Funktion zum Anzeigen der Artikel für alle Tische
-def display_all_items():
-    print("Übersicht aller Tische und ihrer Bestellungen:")
+# Funktion zum Anzeigen der Startseite in der GUI
+def display_start_options_gui():
+    start_options_label = tk.Label(root, text="Startseite:")
+    start_options_label.pack()
+
+    order_button = tk.Button(root, text="Produkt bestellen", command=take_order_gui)
+    order_button.pack()
+
+    bill_button = tk.Button(root, text="Rechnung anzeigen", command=calculate_bill_gui)
+    bill_button.pack()
+
+    change_table_button = tk.Button(root, text="Tisch wechseln", command=display_table_change_options_gui)
+    change_table_button.pack()
+
+    edit_order_button = tk.Button(root, text="Bestellung löschen oder bearbeiten", command=edit_order_gui)
+    edit_order_button.pack()
+
+    display_all_bills_button = tk.Button(root, text="Gesamte Rechnungen anzeigen", command=display_all_bills_gui)
+    display_all_bills_button.pack()
+
+    exit_button = tk.Button(root, text="Programm verlassen", command=root.destroy)
+    exit_button.pack()
+
+# Funktionen zum Anzeigen der Artikel für alle Tische, Optionen für Tischwechsel und Artikel für eine bestimmte Tischnummer in der GUI
+def display_all_items_gui():
+    all_items_label = tk.Label(root, text="Übersicht aller Tische und ihrer Bestellungen:")
+    all_items_label.pack()
+
     for table, items in orders.items():
-        print(f"Tisch {table}:")
+        table_label = tk.Label(root, text=f"Tisch {table}:")
+        table_label.pack()
+
         for index, (category, item) in enumerate(items, start=1):
-            print(f"   {index}. {item} aus der Kategorie {category}")
-        print()
+            item_label = tk.Label(root, text=f"{index}. {item} aus der Kategorie {category}")
+            item_label.pack()
 
-# Funktion zum Ändern des Tisches (Tisch wechseln oder löschen)
-def change_table_options():
-    display_table_change_options()
-    option = input("Geben Sie Ihre Option für den Tischwechsel ein: ")
+        empty_line_label = tk.Label(root, text="")
+        empty_line_label.pack()
 
-    if option == "1":
-        change_table()
-    elif option == "2":
-        change_table_name()
-    elif option == "3":
-        delete_table()
-    elif option == "4":
-        return_to_start()
+def display_table_change_options_gui():
+    table_change_options_label = tk.Label(root, text="Optionen für Tischwechsel:")
+    table_change_options_label.pack()
+
+    change_table_button = tk.Button(root, text="Tisch wechseln", command=change_table_gui)
+    change_table_button.pack()
+
+    change_table_name_button = tk.Button(root, text="Name des Tisches ändern", command=change_table_name_gui)
+    change_table_name_button.pack()
+
+    delete_table_button = tk.Button(root, text="Tisch löschen", command=delete_table_gui)
+    delete_table_button.pack()
+
+    return_to_start_button = tk.Button(root, text="Zurück zur Startseite", command=return_to_start_gui)
+    return_to_start_button.pack()
+
+def display_items_for_table_gui(table):
+    if table in orders:
+        table_label = tk.Label(root, text=f"Bestellungen für Tisch {table}:")
+        table_label.pack()
+
+        for index, (category, item) in enumerate(orders[table], start=1):
+            item_label = tk.Label(root, text=f"{index}. {item} aus der Kategorie {category}")
+            item_label.pack()
     else:
-        print("Ungültige Option für den Tischwechsel.")
+        messagebox.showinfo("Fehler", f"Die Tischnummer {table} existiert nicht.")
 
-# Funktion zum Berechnen der Rechnung für einen Tisch
-def calculate_bill():
+# Funktionen zum Anzeigen aller Rechnungen und zur Rückkehr zur Startseite in der GUI
+def display_all_bills_gui():
+    for table, items in orders.items():
+        bill_label = tk.Label(root, text=f"Rechnung für Tisch {table}:")
+        bill_label.pack()
+
+        for category, item in items:
+            price = menu[category][item]
+            item_label = tk.Label(root, text=f"{item}: {price} Toman")
+            item_label.pack()
+
+        total_bill = sum(menu[category][item] for category, item in items)
+        total_bill_label = tk.Label(root, text=f"Gesamtbetrag: {total_bill} Toman")
+        total_bill_label.pack()
+
+        empty_line_label = tk.Label(root, text="")
+        empty_line_label.pack()
+
+def return_to_start_gui():
+    save_orders_to_file()
+    root.destroy()
+    main()
+
+# Funktionen zum Bestellen von Produkten, Berechnen der Rechnung, Ändern des Tisches, Ändern des Tischnamens, Löschen des Tisches und Bearbeiten der Bestellung
+def take_order_gui():
+    table_number = input("Geben Sie die Tischnummer ein: ")
+
+    while True:
+        display_menu()
+        category_number = input("Geben Sie die Nummer der Kategorie ein ('0' zum Speichern und Beenden): ")
+
+        if category_number == '0':
+            break
+
+        item_number = input("Geben Sie die Nummer des Produkts ein: ")
+
+        category = list(menu.keys())[int(category_number) - 1]
+        item = list(menu[category].keys())[int(item_number) - 1]
+
+        orders.setdefault(table_number, []).append((category, item))
+        print(f"{item} aus der Kategorie {category} wurde zum Tisch {table_number} hinzugefügt.")
+
+
+def calculate_bill_gui():
     print("Übersicht aller offenen Tische:")
     for table, items in orders.items():
         total_bill = sum(menu[category][item] for category, item in items)
@@ -118,8 +177,7 @@ def calculate_bill():
     else:
         print(f"Die Tischnummer {table} existiert nicht.")
 
-# Funktion zum Wechseln des Tisches
-def change_table():
+def change_table_gui():
     global orders
     current_table = input("Geben Sie die aktuelle Tischnummer ein: ")
     new_table = input("Geben Sie die neue Tischnummer ein: ")
@@ -130,8 +188,7 @@ def change_table():
     else:
         print(f"Die Tischnummer {current_table} existiert nicht.")
 
-# Funktion zum Ändern des Namens des Tisches
-def change_table_name():
+def change_table_name_gui():
     table = input("Geben Sie die Tischnummer ein, dessen Namen Sie ändern möchten: ")
     if table in orders:
         new_name = input("Geben Sie den neuen Namen für den Tisch ein: ")
@@ -140,8 +197,13 @@ def change_table_name():
     else:
         print(f"Die Tischnummer {table} existiert nicht.")
 
-# Funktion zum Löschen eines Tisches
-def delete_table():
+def display_menu():
+    print("Speisekarte:")
+    for i, (category, items) in enumerate(menu.items(), start=1):
+        print(f"{i}. {category}:")
+        for j, (item, price) in enumerate(items.items(), start=1):
+            print(f"   {j}. {item} - {price} Toman")
+def delete_table_gui():
     table = input("Geben Sie die Tischnummer ein, den Sie löschen möchten: ")
     if table in orders:
         confirm = input(f"Sind Sie sicher, dass Sie Tisch {table} löschen möchten? (ja/nein): ").lower()
@@ -153,18 +215,11 @@ def delete_table():
     else:
         print(f"Die Tischnummer {table} existiert nicht.")
 
-# Funktion zum Zurückkehren zur Startseite
-def return_to_start():
-    print("Zurück zur Startseite.")
-    save_orders_to_file()
-    main()
-
-# Funktion zum Löschen oder Bearbeiten einer Bestellung
-def edit_order():
+def edit_order_gui():
     table = input("Geben Sie die Tischnummer ein, dessen Bestellung Sie bearbeiten oder löschen möchten: ")
     if table in orders:
         while True:
-            display_items_for_table(table)
+            display_items_for_table_gui(table)
             order_index = input("Geben Sie die Nummer der Bestellung ein, die Sie bearbeiten oder löschen möchten ('0' zum Speichern und Beenden): ")
 
             if order_index == '0':
@@ -193,78 +248,11 @@ def edit_order():
     else:
         print(f"Die Tischnummer {table} existiert nicht.")
 
-# Funktion zum Anzeigen aller Rechnungen
-def display_all_bills():
-    for table, items in orders.items():
-        print(f"Rechnung für Tisch {table}:")
-        for category, item in items:
-            price = menu[category][item]
-            print(f"{item}: {price} Toman")
-        total_bill = sum(menu[category][item] for category, item in items)
-        print(f"Gesamtbetrag: {total_bill} Toman")
-        print()
-
-# Funktion zum Speichern der Bestellungen in einer Textdatei
 def save_orders_to_file():
     with open("bestellungen.txt", "w") as file:
         for table, items in orders.items():
             for category, item in items:
                 file.write(f"Tisch {table}: {category} {item}\n")
 
-# Funktion zum Laden der Bestellungen aus einer Textdatei
-def load_orders_from_file():
-    try:
-        with open("bestellungen.txt", "r") as file:
-            orders_data = file.readlines()
-            for order in orders_data:
-                order_parts = order.strip().split(": ")
-                table = order_parts[0].replace("Tisch ", "")
-                category, item = order_parts[1].split(" ")
-                orders.setdefault(table, []).append((category, item))
-    except FileNotFoundError:
-        print("Die Datei 'bestellungen.txt' wurde noch nicht erstellt.")
-
-# Funktion zum Bestellen von Produkten für einen Tisch
-def take_order():
-    table_number = input("Geben Sie die Tischnummer ein: ")
-
-    while True:
-        display_menu()
-        category_number = input("Geben Sie die Nummer der Kategorie ein ('0' zum Speichern und Beenden): ")
-
-        if category_number == '0':
-            break
-
-        item_number = input("Geben Sie die Nummer des Produkts ein: ")
-
-        category = list(menu.keys())[int(category_number) - 1]
-        item = list(menu[category].keys())[int(item_number) - 1]
-
-        orders.setdefault(table_number, []).append((category, item))
-        print(f"{item} aus der Kategorie {category} wurde zum Tisch {table_number} hinzugefügt.")
-
-# Funktion zum Ausführen der Anwendung
-def main():
-    load_orders_from_file()
-    while True:
-        display_start_options()
-        option = input("Geben Sie Ihre Option ein: ")
-
-        if option == "1":
-            take_order()
-        elif option == "2":
-            calculate_bill()
-        elif option == "3":
-            change_table_options()
-        elif option == "4":
-            edit_order()
-        elif option == "5":
-            display_all_bills()
-        elif option == "6":
-            print("Vielen Dank für die Nutzung der Anwendung!")
-            break
-        else:
-            print("Ungültige Option!")
-
 if __name__ == "__main__":
-    main()
+    main_gui()
